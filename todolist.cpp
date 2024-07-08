c5b93230bec5c74f45a2d858592d5275cdd23649
1dhruvsingh <dhruvsinghsir@gmail.com>
2024-07-08 07:26:17 +0530

    Done with add task part
    unable to the run the code ,will have to debug ,and done with the addTask function

--- todolist.cpp
+++ todolist.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
+struct todolist{
+    string task;
+    int id;
+};
+
+int ID ;
 void banner();
 void AddTask();
 void ShowTask();
@@ -20,7 +28,7 @@ int main(){
 
         int choice;
         cout<<"Enter choice "<<endl;
-        cin>>choice;
+        cin>>choice<<endl;
 
         switch (choice)
         {
@@ -28,20 +36,20 @@ int main(){
               AddTask();
               break;
 
-            case 2;
-              ShowTask();
+            case 2:
+              //ShowTask();
               break;
             
-            case 3;
-              SearchTask();
+            case 3:
+             // SearchTask();
               break;
 
-            case 4;
-              DeleteTask();
+            case 4:
+              //DeleteTask();
               break;
             
-            case 5;
-              UpdateTask();
+            case 5:
+              //UpdateTask();
               break;
             
             default:
@@ -51,3 +59,39 @@ int main(){
     }
     return 0;
 }
+
+void banner(){
+    cout<<"___________________"<<endl;
+    cout<<"|  TO DO LIST APP |"<<endl;
+    cout<<"___________________"<<endl;
+}
+void AddTask(){
+    banner();
+    todolist todo;
+    cout<<"Enter new Task"<<endl;
+    cin.get();
+    getline(cin, todo.task);
+    char save;
+    cout<<"Do you want to save this task? (y/n)"<<endl;
+    cin>>save;
+    if(save=='y'){
+        ID++;
+        ofstream fount;
+        fount.open("todo.txt");
+        fount<<ID<<endl;
+        fount<<todo.task<<endl;
+        fount.close();
+
+        char more;
+        cout<<"Do you want to add more tasks? (y/n)"<<endl;
+        cin>>more<<endl;
+        if(more=='y'){
+            AddTask();
+          }
+        else(){
+        cout<<"Added suceedfully"<<endl;
+        }
+    }
+}
+
+
